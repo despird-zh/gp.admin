790510e822fd3b4a73a8e15da1fd40858f7d01ee
despird-zh <despird.zhang@yahoo.com>
2017-03-15 22:17:56 +0800

    draft header from appbar

--- source/js/views/App/HeaderBar.jsx
+++ source/js/views/App/HeaderBar.jsx
@@ -1,60 +1,93 @@
 import React from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin'; 
-import IconMenu from 'material-ui/IconMenu';
 import IconButton from 'material-ui/IconButton';
-import FontIcon from 'material-ui/FontIcon';
-import NavigationExpandMoreIcon from 'material-ui/svg-icons/navigation/expand-more';
-import MenuItem from 'material-ui/MenuItem';
-import DropDownMenu from 'material-ui/DropDownMenu';
-import RaisedButton from 'material-ui/RaisedButton';
-import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+import NavigationMenu from 'material-ui/svg-icons/navigation/menu';
 import muiThemeable from 'material-ui/styles/muiThemeable';
 
 injectTapEventPlugin();
 
-export default class HeaderBar extends React.Component {
+function getStyles(props) {
+  const {
+    appBar,
+    button: {
+      iconButtonSize,
+    },
+    zIndex,
+  } = props.muiTheme;
+
+  const flatButtonSize = 36;
+
+  const styles = {
+    root: {
+      position: 'relative',
+      zIndex: zIndex.appBar,
+      width: '100%',
+      display: 'flex',
+      //backgroundColor: appBar.color,
+      paddingLeft: appBar.padding,
+      paddingRight: appBar.padding,
+    },
+    title: {
+      whiteSpace: 'nowrap',
+      overflow: 'hidden',
+      textOverflow: 'ellipsis',
+      margin: 0,
+      paddingTop: 0,
+      letterSpacing: 0,
+      fontSize: 24,
+      fontWeight: appBar.titleFontWeight,
+      color: appBar.textColor,
+      height: appBar.height,
+      lineHeight: `${appBar.height}px`,
+    },
+    mainElement: {
+      boxFlex: 1,
+      flex: '1',
+    },
+    iconButtonStyle: {
+      marginTop: (appBar.height - iconButtonSize) / 2,
+      marginRight: 8,
+      marginLeft: -16,
+    },
+    iconButtonIconStyle: {
+      fill: appBar.textColor,
+      color: appBar.textColor,
+    },
+    flatButton: {
+      color: appBar.textColor,
+      marginTop: (iconButtonSize - flatButtonSize) / 2 + 1,
+    },
+  };
+
+  return styles;
+}
+
+class HeaderBar extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
       value: 3,
     };
+    this.styles = getStyles(this.props);
   }
 
-
-  handleChange = (event, index, value) => this.setState({value});
-
   render() {
+
+    const titleElement = React.createElement('h1', {
+      style: this.props.muiTheme.prepareStyles(Object.assign({}, this.styles.title, this.styles.mainElement)),
+    }, 'Infinity Connection');
+
     return (
-      <Toolbar className="content">
-        <ToolbarGroup firstChild={true}>
-          <DropDownMenu value={this.state.value} onChange={this.handleChange}>
-            <MenuItem value={1} primaryText="All Broadcasts" />
-            <MenuItem value={2} primaryText="All Voice" />
-            <MenuItem value={3} primaryText="All Text" />
-            <MenuItem value={4} primaryText="Complete Voice" />
-            <MenuItem value={5} primaryText="Complete Text" />
-            <MenuItem value={6} primaryText="Active Voice" />
-            <MenuItem value={7} primaryText="Active Text" />
-          </DropDownMenu>
-        </ToolbarGroup>
-        <ToolbarGroup>
-          <ToolbarTitle text="Options" />
-          <FontIcon className="muidocs-icon-custom-sort" />
-          <ToolbarSeparator />
-          <RaisedButton label="Create Broadcast" primary={true} />
-          <IconMenu
-            iconButtonElement={
-              <IconButton touch={true}>
-                <NavigationExpandMoreIcon />
-              </IconButton>
-            }
-          >
-            <MenuItem primaryText="Download" />
-            <MenuItem primaryText="More Info" />
-          </IconMenu>
-        </ToolbarGroup>
-      </Toolbar>
+      <div style={this.styles.root} className="content">
+        <IconButton style={this.styles.iconButtonStyle} iconStyle={this.styles.iconButtonIconStyle}>
+          <NavigationMenu style={Object.assign({}, this.styles.iconButtonIconStyle)} />
+        </IconButton>
+        {titleElement}
+      </div>
     );
   }
 }
+
+
+export default muiThemeable()(HeaderBar);
