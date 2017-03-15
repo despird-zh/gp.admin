647d16055707f5cb582bb85b8bccda26d8a0fd7d
despird-zh <despird.zhang@yahoo.com>
2017-03-15 09:49:12 +0800

    update test

--- source/js/views/App/index.jsx
+++ source/js/views/App/index.jsx
@@ -3,21 +3,37 @@ import { Link, IndexLink } from 'react-router';
 import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
 
 import HeaderBar from './HeaderBar';
 import FooterBar from './FooterBar';
+
+const rootTheme = getMuiTheme(lightBaseTheme);
+const styles = {
+  header: {
+    background: rootTheme.toolbar.backgroundColor,
+  },
+  footer: {
+    background: lightBaseTheme.palette.accent2Color,
+  }
+};
 
 // This is a class-based component because the current
 // version of hot reloading won't hot reload a stateless
 // component at the top-level.
 class App extends Component {
 
+
   render() {
     return (
-      <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+      <MuiThemeProvider muiTheme={ rootTheme }>
         <div className="wrapper">
-          <div className="header">
+          <div  style={ styles.header } className="header">
             <div className="content">
+
+            <AppBar
+                title="Title"
+              />
             <HeaderBar/>
             </div>
           </div>
@@ -26,7 +42,7 @@ class App extends Component {
               {this.props.children}
             </div>
           </div>
-          <div className="footer">
+          <div style={ styles.header } className="footer">
             <FooterBar/>
           </div>
       </div>
