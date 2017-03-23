3be6af7716835a51b4dd18dd48c05b035a330f65
despird-zh <despird.zhang@yahoo.com>
2017-03-24 00:48:23 +0800

    update settings

--- source/js/views/config/SettingPage.jsx
+++ source/js/views/config/SettingPage.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import Divider from 'material-ui/Divider';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import IconButton from 'material-ui/IconButton';
+import ModeEditIcon from 'material-ui/svg-icons/editor/mode-edit';
 import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
 
 const styles = {
@@ -9,7 +13,6 @@ const styles = {
     marginTop: 10
   },
   spacer: { flex: 1 },
-
 }
 export default class SettingPage extends React.Component {
 
@@ -30,21 +33,29 @@ export default class SettingPage extends React.Component {
   	return (
 		  <div >
 		  	<div style={styles.root}>
-		  	aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+		  	   <TextField hintText="Hint Text"/>
+          <div style={styles.spacer}/>
+          <div>
+              <RaisedButton label="Refresh" style={{margin: 4}} onTouchTap ={this.handleRefresh}/>
+              <RaisedButton label="Save" primary={true} style={{margin: 4}} />
+          </div>
   			</div>
+
         <Table>
           <TableHeader>
             <TableRow>
               <TableHeaderColumn>ID</TableHeaderColumn>
               <TableHeaderColumn>Name</TableHeaderColumn>
-              <TableHeaderColumn>Status</TableHeaderColumn>
+              <TableHeaderColumn>OP.</TableHeaderColumn>
             </TableRow>
           </TableHeader>
           <TableBody>
             <TableRow>
               <TableRowColumn>1</TableRowColumn>
               <TableRowColumn>John Smith</TableRowColumn>
-              <TableRowColumn>Employed</TableRowColumn>
+              <TableRowColumn>
+                <IconButton iconStyle={{height:20}}><ModeEditIcon/></IconButton >
+              </TableRowColumn>
             </TableRow>
             <TableRow>
               <TableRowColumn>2</TableRowColumn>
