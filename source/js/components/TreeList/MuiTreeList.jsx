a28a9d3512105673c7d434eb162b474c0ad5b07a
despird-zh <despird.zhang@yahoo.com>
2017-04-21 12:36:53 +0800

    update the org hierarchy

--- source/js/components/TreeList/MuiTreeList.jsx
+++ source/js/components/TreeList/MuiTreeList.jsx
@@ -3,16 +3,18 @@ import PropTypes from 'prop-types'
 
 import { List,ListItem } from 'material-ui/List';
 import TextField from 'material-ui/TextField'
-import OpenIcon from 'material-ui/svg-icons/navigation/expand-more'
-import CloseIcon from 'material-ui/svg-icons/navigation/expand-less'
+
 import FolderIcon from 'material-ui/svg-icons/file/folder'
 import FileIcon from 'material-ui/svg-icons/editor/insert-drive-file'
-
-import ActionGrade from 'material-ui/svg-icons/action/grade';
-import ContentInbox from 'material-ui/svg-icons/content/inbox';
-import ContentDrafts from 'material-ui/svg-icons/content/drafts';
-import ContentSend from 'material-ui/svg-icons/content/send';
 import Subheader from 'material-ui/Subheader';
+
+const MuiIcons = require('material-ui/svg-icons');
+
+const styles = {
+  innerDivStyle:{
+    paddingLeft: 48
+  }
+};
 
 class MuiTreeList extends Component {
   constructor(props) {
@@ -23,64 +25,101 @@ class MuiTreeList extends Component {
     this.handleTouchTap = this.handleTouchTap.bind(this)
   }
 
-  handleTouchTap(listItem, index) {
-     
+  handleTouchTap(e) {
+     console.log(' touch tap ======~~~~~');
+     console.log(e);
   }
 
-  renderTreeNode(child, index, depth = 0, pNodePath) {
-
-    const {useFolderIcons} = this.props;
-    const nodePath = (pNodePath) ? `${pNodePath}-${child.key}` : `${child.key}`;
-    
-    const cloneProps = {
-      ref: `treeNode-${nodePath}`,
-      root: this,
-      nodePath,
-      depth,
-      useFolderIcons
-    };
-
-    return React.cloneElement(child, cloneProps);
+  handleNestedToggle(e, nodeItem) {
+     console.log(' nest toggle ======~~~~~');
+     console.log(nodeItem);
   }
 
   render() {
 
+    const getLeftIcon = (nodeItem) => {
+
+      let { icon } = nodeItem;
+      let { useFolderIcons } = this.props;
+
+      if ( useFolderIcons && !icon) {
+        if (nodeItem.children) {
+            return <FolderIcon />
+        } else {
+            return <FileIcon />
+        }
+      } else {
+        let SpecificIcon = MuiIcons[ icon ];
+        return <SpecificIcon/>;
+      }
+    }
+
     const loopNodes = data => {
       return data.map((item) => {
+        let leftIcon = getLeftIcon( item );
 
         if (item.children) {
+
           let nestedNodes = loopNodes (item.children);
           return (
-            <ListItem 
+            <MuiTreeItems 
               key={ item.key } 
-              primaryText={item.title}
               style={ this.props.itemStyle }
-              leftIcon={<ActionGrade />}
+              leftIcon={leftIcon}
+              nodeItem={ item }
+              innerDivStyle={ styles.innerDivStyle }
               primaryTogglesNestedList={true}
-              nestedItems={ nestedNodes }
-            />
+              onNestedListToggle={ this.handleNestedToggle }
+              primaryText={ item.title }
+              nestedItems={ nestedNodes } >
+              </MuiTreeItems>
           );
         }
-        return <MuiTreeItem key={ item.key } nodeItem ={ item }leftIcon={<ContentSend />} primaryText={item.title} style={ this.props.itemStyle }/>;
+
+        return (
+          <MuiTreeItem 
+            key={ item.key } 
+            leftIcon={ leftIcon } 
+            nodeItem={ item }
+            onTouchTap={ this.handleTouchTap }
+            innerDivStyle={ styles.innerDivStyle }
+            primaryText={ item.title }
+            style={ this.props.itemStyle } >
+          </MuiTreeItem>
+        );
       });
     };
 
     const rootChildren = loopNodes(this.state.nodes);
 
     return (
-      <List>
-        <Subheader>Nested List Items</Subheader>
+      <List style={this.props.style}>
         {rootChildren}
       </List>
     );
   }
 }
 
-const MuiTreeItem = ({nodeItem, ...rest }) => {
-  console.log(nodeItem);
-  return (<ListItem
-            {...rest}
-          />);
+const MuiTreeItem = ({nodeItem, onTouchTap, ...rest }) => {
+  const handleTouchTap = ()=>{
+    if(onTouchTap) onTouchTap(nodeItem);
+  }
+  return (
+    <ListItem
+      onTouchTap={ handleTouchTap }
+      {...rest}
+    />);
+};
+
+const MuiTreeItems = ({nodeItem, onNestedListToggle, ...rest }) => {
+  const handleNestedListToggle = (e)=>{
+    if(onNestedListToggle) onNestedListToggle(e, nodeItem);
+  }
+  return (
+    <ListItem
+      onNestedListToggle={ handleNestedListToggle }
+      {...rest}
+    />);
 };
 
 export default MuiTreeList
--- source/js/views/master/OrgHierPage.jsx
+++ source/js/views/master/OrgHierPage.jsx
@@ -1,29 +1,66 @@
 import React from 'react';
 import SocialGrp from 'material-ui/svg-icons/social/group';
-import MuiTreeList from '../../components/TreeList/MuiTreeList'
-import MuiTreeItem from '../../components/TreeList/MuiTreeItem'
+import MuiTreeList from '../../components/TreeList/MuiTreeList';
+import Divider from 'material-ui/Divider';
+import RaisedButton from 'material-ui/RaisedButton';
+import {List, ListItem, makeSelectable} from 'material-ui/List';
+import IconButton from 'material-ui/IconButton';
+import ContentClear from 'material-ui/svg-icons/content/clear';
+import ContentRemove from 'material-ui/svg-icons/content/remove';
+import Avatar from 'material-ui/Avatar';
+import muiThemeable from 'material-ui/styles/muiThemeable';
+
 var nodes = [
   {
     key: 'k1',
-    title: 'Title1'
+    title: 'Title1',
+    icon: 'ActionSupervisorAccount',
   },{
     key: 'k2',
-    title: 'Title2',
+    title: 'Title2 (5/41)',
+    icon: 'SocialPeople',
     children: [
       {
         key: 'k21',
-        title: 'Title21'
+        title: 'Title21',
+        icon: 'ActionSupervisorAccount',
       },
       {
         key: 'k22',
-        title: 'Title22'
+        title: 'Title22',
+        icon: 'ActionSupervisorAccount',
       }
     ]
   }
 ];
 
+const SelectableList = makeSelectable(List);
 
-
+const getStyles = (muiTheme) => {
+  const { baseTheme: { spacing, palette } } = muiTheme;
+  return {
+    rootStyle: {
+      display: 'flex'
+    },
+    spacer: { flex: 1 },
+    topFull: {
+      flexBasis: '100%',
+      display: 'flex', 
+      marginTop: 10
+    },
+    panelTitle: {
+      color: palette.secondaryTextColor,
+      fontSize: 16, 
+      marginBottom: spacing.desktopGutterMini,
+      marginTop: spacing.desktopGutterMini,
+    },
+    halfStyle: {
+      flexBasis: '50%',
+      paddingLeft: spacing.desktopGutterMini,
+      paddingRight: spacing.desktopGutterMini
+    }
+  };
+}
 class OrgHierPage extends React.Component { 
 
   constructor(props, context) {
@@ -36,14 +73,65 @@ class OrgHierPage extends React.Component {
   }
 
   render() {
-   
+    let styles = getStyles(this.props.muiTheme);
+
     return (
-      <div style={{width: 400}}>
-        <MuiTreeList nodes={nodes} useFolderIcons={true}>
-        </MuiTreeList>
+      <div >
+        <div style={styles.topFull}>
+          <div style={styles.spacer}/>
+          <div>
+              <RaisedButton label="Refresh" style={{margin: 4}} onTouchTap ={this.handleRefresh}/>
+          </div>
+        </div>
+        <div style={ styles.rootStyle }>
+          <div style={ styles.halfStyle }>
+            <h3 style={ styles.panelTitle }>Hierarchy </h3>
+            <Divider/>
+            <MuiTreeList nodes={ nodes } useFolderIcons={true}>
+            </MuiTreeList>
+          </div>
+          <div style={ styles.halfStyle }>
+            <h3 style={ styles.panelTitle }>Members </h3>
+            <Divider/>
+            <SelectableList
+                value={ 1 }
+                onChange={this.handleMemberChange}>
+                <ListItem
+                  primaryText="Brendan Lim"
+                  leftAvatar={<Avatar src="assets/img/ok-128.jpg" />}
+                  rightIconButton={<IconButton onTouchTap={ this.handleMemberDelete }><ContentClear/></IconButton>}
+                  value={1}
+                />
+                <ListItem
+                  primaryText="Eric Hoffman"
+                  leftAvatar={<Avatar src="assets/img/kolage-128.jpg" />}
+                  rightIconButton={<IconButton onTouchTap={ this.handleMemberRemove }><ContentRemove/></IconButton>}
+                  value={2}
+                />
+                <ListItem
+                  primaryText="Grace Ng - 永东科技"
+                  leftAvatar={<Avatar src="assets/img/uxceo-128.jpg" />}
+                  rightIconButton={<IconButton onTouchTap={ this.handleMemberDelete }><ContentClear/></IconButton>}
+                  value={3}
+                />
+                <ListItem
+                  primaryText="Kerem Suer"
+                  leftAvatar={<Avatar src="assets/img/kerem-128.jpg" />}
+                  rightIconButton={<IconButton onTouchTap={ this.handleMemberDelete }><ContentClear/></IconButton>}
+                  value={4}
+                />
+                <ListItem
+                  primaryText="Raquel Parrado"
+                  leftAvatar={<Avatar src="assets/img/raquelromanp-128.jpg" />}
+                  rightIconButton={<IconButton onTouchTap={ this.handleMemberDelete }><ContentClear/></IconButton>}
+                  value={5}
+                />
+              </SelectableList>
+            </div>
+          </div>
       </div>
     );
   };
 }
 
-export default OrgHierPage;
+export default muiThemeable()(OrgHierPage);
